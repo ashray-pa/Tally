daf18d68633db10fdb5f8f1ac04099e908ad9177
Saurav Kumar <43887196+singhsaurav23@users.noreply.github.com>
2022-07-26 12:08:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ class Utilities:
         return 'POST HTTP/1.1 \r\nContent-Type: text/html\r\nContent-Length: %d\r\nReq-Type: %s\r\n\r\n%s' %(len(data), req_type, data)
 
     def post_res(self, res_type, data):
-        return 'POST HTTP/1.1 \r\nContent-Type: text/html\r\nContent-Length: %d\r\nRes-Type: %s\r\n\r\n%s' %(len(data), res_type, data)
+        return 'HTTP/1.1 \r\nContent-Type: text/html\r\nContent-Length: %d\r\nRes-Type: %s\r\n\r\n%s' %(len(data), res_type, data)
 
     def send_ack(self, socket):
-        socket.send('GET HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nRes-Type: ack'.encode())
+        socket.send('HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nRes-Type: ack'.encode())
