e7eb9692922cb6645512073877737ff5fafdcc7a
mangkp06 <58565726+mangkp06@users.noreply.github.com>
2022-08-17 16:55:26 +0530

    Update chat_app_gui.py

--- chat_app_gui.py
+++ chat_app_gui.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from http import server
 import threading
 import tkinter
 from tkinter import simpledialog
@@ -12,30 +13,56 @@ HOST = "127.0.1.1"
 PORT = 9095
 utils = Utilities()
 
+
+
 class ChatApp:
     def __init__(self):
-        self.conn = Connection(HOST, PORT)
-        self.conn.connect_to_server()
-        msg = tkinter.Tk()
-        msg.withdraw()
-        self.name = simpledialog.askstring(
-            "Name", "Enter your name", parent=msg)
-        self.client_id = self.conn.recvId().split('\r\n\r\n')[1]
-        self.client_id=self.client_id.split("\r\n")[0]
+        #self.conn = Connection(HOST, PORT)
+        #self.conn.connect_to_server()
+       
+        self.reconnection()
+        if(self.conn.running):
+            msg = tkinter.Tk()
+            msg.withdraw()
+            self.name = simpledialog.askstring(
+                "Name", "Enter your name", parent=msg)
 
-        self.gui_done = False
-        self.conn.running = True
 
         # gui_thread = threading.Thread(target=self.gui_)
-        recv_thread = threading.Thread(target=self.read_)
-        down_thread = threading.Thread(target=self.ping_)
-        # gui_thread.daemon = True
-        recv_thread.daemon = True
-        down_thread.daemon = True
-        # gui_thread.start()
-        recv_thread.start()
-        down_thread.start()
+        # recv_thread = threading.Thread(target=self.read_)
+        # down_thread = threading.Thread(target=self.ping_)
+        # # gui_thread.daemon = True
+        # recv_thread.daemon = True
+        # down_thread.daemon = True
+        # # gui_thread.start()
+        # recv_thread.start()
+        # down_thread.start()
 
+
+    def reconnection(self):
+        
+        self.conn=Connection(HOST,PORT)
+        self.conn.connect_to_server()
+        if self.conn.running:
+            self.client_id = self.conn.recvId().split('\r\n\r\n')[1]
+            self.client_id=self.client_id.split("\r\n")[0]
+            print("the new client id is ",self.client_id)
+            recv_thread = threading.Thread(target=self.read_)
+            down_thread = threading.Thread(target=self.ping_)
+        # gui_thread.daemon = True
+            recv_thread.daemon = True
+            down_thread.daemon = True
+            # gui_thread.start()
+            recv_thread.start()
+            down_thread.start()
+        else:
+            self.conn.server_down_close()
+
+             
+        
+          
+
+        #print("i have no clue")
 
     def gui_(self):
         self.window = tkinter.Tk(className=self.name)
@@ -78,9 +105,11 @@ class ChatApp:
     def write_(self):
         message = f"{self.name}: {self.msg_input.get('1.0', 'end')}"
         try:
-            self.conn.sendMessage(message, str(datetime.now()), self.client_id)
+            if self.conn.running:
+                self.conn.sendMessage(message, str(datetime.now()), self.client_id)
         except BrokenPipeError:
             print("----- server is down -----")
+            
             self.server_down_cnt = self.server_down_cnt - 1
             if self.server_down_cnt <= 0:
                 self.conn.server_down_close()
@@ -99,7 +128,7 @@ class ChatApp:
                     for line in mess_.split('\r\n'):
                         if(line.split(': ')[0] == 'Res-Type'):
                             res_type = line.split(': ')[1]
-                    if self.gui_done:
+                    if True:
                         if res_type == 'ack':
                             print('HTTP/1.1 200 OK message sent')
                             self.notify.place(relx=1.0, rely=0.0, anchor = 'ne')
@@ -131,11 +160,14 @@ class ChatApp:
                         utils.send_ack(socket=self.conn.sock, isMsg=True, dt=msg_sent_time, id=client_id)
 
             except ConnectionAbortedError:
+                print("here 163")
                 break
             except Exception as e:
-                print(e)
-                self.conn.closeConn()
+                print("please handle this error",e)
+                
                 exit(0)
+                #self.conn.closeConn()
+                
 
 
     def stop_(self):
@@ -148,15 +180,23 @@ class ChatApp:
 
 
     def ping_(self):
-        while True:
+        while self.conn.running:
             self.conn.ping_()
             try:
-                if self.conn.server_status == "down": 
-                    self.window.destroy()
-                    exit(0)
+                if self.conn.server_status == "down":
+                    
+                    self.conn.closeConn() 
+                    self.reconnection()     
+                    
+                    if(self.conn.running==False):     
+                        self.window.destroy() 
+                        exit(0)        
             except:
                 print('---client closed---')
+                self.window.destroy()
                 exit(0)
+
+            
 
 gui = ChatApp()
 gui.gui_()
